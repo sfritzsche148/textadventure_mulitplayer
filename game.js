17c798641ad65dacd3d5debaeddd49a9b84e646b
Sebastian Fritzsche <sebifr58@gmx.de>
2023-01-17 20:30:00 +0100

    update

--- game.js
+++ game.js
@@ -1,14 +1,16 @@
 let btn = document.getElementById('btn');
 
 
+
 btn.addEventListener('click', function() {
     let input = document.getElementById('inp');
     let content = document.getElementById('content');
 
     let inp1 = input.value;
 
-    let playername = inp1;
 
+
+    // Player Blueprint
     class Player {
         constructor(name, hp, inventory) {
             this.name = name;
@@ -17,11 +19,15 @@ btn.addEventListener('click', function() {
         }
     }
 
+    // Playername
+    let playername = inp1;
+
     let playerinventory = [];
     let playerhp = 100
 
-    let player = new Player(playername, playerhp, playerinventory); 
+    let player = new Player(playername, playerhp, playerinventory);
 
+       
     let story = {
         'question1': {
             title: 'Chapter 1',
@@ -30,19 +36,43 @@ btn.addEventListener('click', function() {
                 backward: 'Go Backward'
             }
         },
-
+    
         'question2': {
             title: 'Chapter 1',
-            answers: {
-                'toforward': 'You see a lake.',
-                'tobackward': 'You see a clif'
-            },
-        }
-    }
+            text: {
+                toforward1: {
+                    answer: 'You see a big lake',
+                    options: {
+                        forward2: 'Go Forward',
+                        inspect1: 'Inspect'
+                    }
+                },
+                tobackward1: {
+                    answer: 'You see a clif',
+                    options: {
+                        forward2: 'Go forward',
+                        inspect1: 'Inspect'
+                    }
+                }
+            }
+        },
 
+        'question3': {
+            title: 'Chapter 1',
+            text: {
+
+            }
+
+        }
+
+    }
 
     content.innerHTML = `
     <p>${player.hp}</p>
+    <img
+    src="./coin.png"
+    alt="" 
+    />
     <p>Hello ${player.name}</p>
     <h2>${story.question1.title}</h2>
     <button id='Forwardbutton'>${story.question1.options.forward}</button>
@@ -55,25 +85,26 @@ btn.addEventListener('click', function() {
     ForwardBtn.addEventListener('click', function() {
         content.innerHTML = `
         <h2>${story.question2.title}</h2>
-        <p>${story.question2.answers.toforward}</p>
+        <p>${story.question2.text.toforward1.answer}</p>
+        <button>${story.question2.text.toforward1.options.forward2} </button>
+        <button>${story.question2.text.toforward1.options.inspect1} </button>
         `
+
+        
     })
 
     BackBtn.addEventListener('click', function() {
         content.innerHTML = `
         <h2>${story.question2.title}</h2>
-        <p>${story.question2.answers.tobackward}</p>
+        <p>${story.question2.text.tobackward.answer}</p>
+        <button>${story.question2.text.tobackward1.options.forward2} </button>
+        <button>${story.question2.text.tobackward1.options.inspect1} </button>
         `
     })
 
-    let pickbtn = document.querySelector('#pickupbutton');
-
     function Checkpoint1() {
         content.innerHTML = `
-        <p>You see big tres</p>
+        <p>Checkpoint</p>        
         `
-
     }
-
-
 })
