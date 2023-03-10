77a40d55d1135bef3052752cf6a71b1b1408f1d6
Sebastian Fritzsche <sebifr58@gmx.de>
2023-03-10 19:06:11 +0100

    refactoring

--- game.js
+++ game.js
@@ -1,14 +1,11 @@
 let btn = document.getElementById('btn');
 
 
-
 btn.addEventListener('click', function() {
     let input = document.getElementById('inp');
     let content = document.getElementById('content');
 
     let inp1 = input.value;
-
-
 
     // Player Blueprint
     class Player {
@@ -27,84 +24,31 @@ btn.addEventListener('click', function() {
 
     let player = new Player(playername, playerhp, playerinventory);
 
-       
-    let story = {
-        'question1': {
-            title: 'Chapter 1',
-            options: {
-                forward: 'Go Forward',
-                backward: 'Go Backward'
-            }
-        },
-    
-        'question2': {
-            title: 'Chapter 1',
+
+    let rooms = {
+        'room1': {
             text: {
-                toforward1: {
-                    answer: 'You see a big lake',
-                    options: {
-                        forward2: 'Go Forward',
-                        inspect1: 'Inspect'
-                    }
+                title: 'Room 1',
+                description: 'You are in the First Room.',
+                objects: {
+                   key: 'You Found the key for the next door'
                 },
-                tobackward1: {
-                    answer: 'You see a clif',
-                    options: {
-                        forward2: 'Go forward',
-                        inspect1: 'Inspect'
-                    }
+                options: {
+                    inspect: 'Inspect',
+                    nextRoom: 'Go to the next Room', 
                 }
             }
-        },
+        }, 
+        'room2': {
 
-        'question3': {
-            title: 'Chapter 1',
-            text: {
-
-            }
-
-        }
-
+        },        
     }
 
+    // room1
     content.innerHTML = `
-    <p>${player.hp}</p>
-    <img
-    src="./coin.png"
-    alt="" 
-    />
-    <p>Hello ${player.name}</p>
-    <h2>${story.question1.title}</h2>
-    <button id='Forwardbutton'>${story.question1.options.forward}</button>
-    <button id='Backwardbutton'>${story.question1.options.backward}</button>
+    <h2>${rooms.room1.text.title}</h2>
+    <p>${rooms.room1.text.description}</p>
+    <button>${rooms.room1.text.options.inspect}</button>
+    <button>${rooms.room1.text.options.nextRoom}</button>
     `
-
-    let ForwardBtn = document.querySelector('#Forwardbutton');
-    let BackBtn = document.querySelector('#Backwardbutton');
-
-    ForwardBtn.addEventListener('click', function() {
-        content.innerHTML = `
-        <h2>${story.question2.title}</h2>
-        <p>${story.question2.text.toforward1.answer}</p>
-        <button>${story.question2.text.toforward1.options.forward2} </button>
-        <button>${story.question2.text.toforward1.options.inspect1} </button>
-        `
-
-        
-    })
-
-    BackBtn.addEventListener('click', function() {
-        content.innerHTML = `
-        <h2>${story.question2.title}</h2>
-        <p>${story.question2.text.tobackward.answer}</p>
-        <button>${story.question2.text.tobackward1.options.forward2} </button>
-        <button>${story.question2.text.tobackward1.options.inspect1} </button>
-        `
-    })
-
-    function Checkpoint1() {
-        content.innerHTML = `
-        <p>Checkpoint</p>        
-        `
-    }
 })
